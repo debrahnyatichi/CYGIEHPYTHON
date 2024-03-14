52f99b4f23aa8f641b4bfdac36b8f42e6316f28b
debrahnyatichi <binyanyadebby@gmail.com>
2024-03-14 16:38:04 +0300

    Python Installation

---
+++ PythonIntro.py
@@ -0,0 +1,77 @@
+#                   INTRODUCTION
+#Python is a high-level object-oriented programming language.
+# It was developed by Guido Van Rossum in 1989 and released in 1991.
+# It is often called a batteries included language due to its complex standard library.
+# The name python was taken from popular BBC comedy show of that time "Monty Python's Flying Circus"
+#
+#                   FEATURES OF PYTHON
+# 1.Easy to learn and use - it has a simple structure and clearly defined structure,
+# allowing the learners to pick up the language quickly.
+# 2.Cross-platform - it can run on different OS such as windows or linux, making it portable.
+# 3.Free and open source - python is freely available at official website
+# It's open source hence the source code is publicly available
+# 4.Object-oriented approach-  it uses an object-oriented approach that encapsulates code within objects.
+# 5.Large Standard Library -  it provides various libraries for various tasks such as ML,web development and data analysis.
+# 6. Memory management -  You don't need to assign the data type of the variable, When you assign some value to the variable it
+# automatically allocates the memory to the variable at one runtime.
+#
+#                       USES OF PYTHON
+# Used to develop desktop application
+# Used to develop web Application
+# Used in the field of Data Science, Machine Learning and Artificial Intelligence to analyze data, buld predictive models and make business decisions.
+#Used in game development.
+#
+#                PYTHON FRAMEWORKS AND LIBRARIES
+# For web development -  Django, Flask, CherryPy and Pyramid
+# For GUI based applications - Tk, PyGTK, PyJs
+# For Machine Learning - TensorFlow, PyTorch,Scikit,Matplotlib, SciPy
+# For Mathematical computations - numpy and pandas
+#
+#                   BEST PYTHON IDEs
+# Jupyter Notebook from Anaconda distribution
+# Visual Studio Code from microsoft
+# Pycharm
+# Python IDLE
+# Spyder Notebook
+# Sublime text a text editor
+#
+#                     TOP COMPANIES USING PYTHON
+# Netflix
+# Google
+# Quora
+# Facebook
+# Instagram
+# Spotify
+#
+#                       INSTALLING PYTHON
+# Download a python Interpreter in python.org - download latest
+# After the installer is downloaded open it - installation window pops up
+# Check the 2boxes for path and launcher then click on "install now"
+# A window of "setup was successful appears" click on close.
+# Check whether it was installed successfully by using this command in cmd "python --version"
+# To run a basic command type "py" and press enter
+# print("Hello World!") and press enter
+#
+#               TO INSTALL LIBRARIES OF PYTHON
+# Make sure the latest pip is installed in your laptop type "pip --version" and press enter
+# To upgrade pip use this command "python -m pip install --upgrade pip"
+# Install numpy using this command "pip install numpy" and press enter wait until its successfully downloaded
+# To install scipy library "pip install scipy"
+# To download and install pandas library "pip install pandas"
+# To install matplotlib library "pip install matplotlib"
+# To check if libraries are successfully installed:
+#       Type "python" and press enter
+#       "import numpy as  np"
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
