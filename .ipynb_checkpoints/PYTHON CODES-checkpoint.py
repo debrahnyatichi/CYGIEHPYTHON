40dbbbdbb3071d8a21519a7d42abfce379540aef
debrahnyatichi <binyanyadebby@gmail.com>
2024-03-24 19:01:27 +0300

    Simple addition code

--- .ipynb_checkpoints/PYTHON CODES-checkpoint.py
+++ .ipynb_checkpoints/PYTHON CODES-checkpoint.py
@@ -4,13 +4,17 @@ import scipy
 import matplotlib
 import pandas
 
+#simple Hello World
 print("Hello World!")
 
+#Array Code
 a = np.array([[1, 2, 4], [5, 7, 9]], dtype='float')
 print("array created:\n", a)
 
+#Simple print Kenya command
 print("Kenya")
 
+#simple division code
 a=5
 b=10
 c=b/a
--- PYTHON CODES.py
+++ PYTHON CODES.py
@@ -4,14 +4,24 @@ import scipy
 import matplotlib
 import pandas
 
+#simple Hello World
 print("Hello World!")
 
+#Array Code
 a = np.array([[1, 2, 4], [5, 7, 9]], dtype='float')
 print("array created:\n", a)
 
+#Simple print Kenya command
 print("Kenya")
 
+#Dividing 2 numbers
 a=5
 b=10
 c=b/a
+print(c)
+
+#adding 2 numbers
+a=7
+b=8
+c=a+b
 print(c)
