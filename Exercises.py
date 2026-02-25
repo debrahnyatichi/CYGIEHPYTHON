f762cf8d740b0626c1e60391e08ee576e7d25d56
debrahnyatichi <binyanyadebby@gmail.com>
2026-02-25 17:50:34 +0300

    arithmetics&maths

--- Exercises.py
+++ Exercises.py
@@ -14,3 +14,23 @@ quantity = int(input("How many would you like?: "))
 total = price * quantity
 print(f"You have bought {quantity} x {item}/s")
 print(f"Your total is: ${total}")
+
+#Exercise 3: calculate the circumference of a circle
+
+import math
+
+radius = float(input("Enter the radius: "))
+circumference = 2 * math.pi * radius
+print(f" The circumference is: {round(circumference, 2)}cm")
+
+#Exercise 4: calculate the area of a circle
+
+radius = float(input("Enter the radius: "))
+area = math.pi * radius ** 2
+print(f" The area is: {round(area, 2)}cm^2")
+
+# Exercise 5: Calculate the Hypotenuse of a right triangle
+a = float(input("Enter the a: "))
+b = float(input("Enter the b: "))
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+print(f" Answer is: {round(c, 2)} cm")
