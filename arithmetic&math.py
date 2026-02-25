0204e56d1ddefe4862a1f39bcc509ae435203728
debrahnyatichi <binyanyadebby@gmail.com>
2026-02-25 16:26:43 +0300

    arithmetics&maths

---
+++ arithmetic&math.py
@@ -0,0 +1,48 @@
+friends = 50
+
+#addition
+#friends = friends + 1
+#friends += 1
+
+#subtraction
+# friends = friends - 2
+#friends -= 2
+
+#multiplication
+#friends = friends * 2
+#friends *= 3
+
+#division
+#friends = friends / 2
+#friends /= 2
+
+#Power of
+#friends = friends ** 2
+#friends **= 2
+
+#modulus
+#remainder = friends % 3
+print(friends)
+
+#math
+x = 3.14
+y = 4
+z = 5
+
+#round function result = round(x)
+#absolute value function result = abs(y)
+#power function result = pow(4, 3)
+#max and min function
+# result = max(x, y, z)
+result = min(x, y, z)
+print(result)
+
+#Useful Constants and functions in math class
+import math
+x = 9.9
+print(math.pi)
+print(math.e) #exponential constant
+#result = math.sqrt(x) #sqrt function
+#result = math.ceil(x) #rounds a number up
+result = math.floor(x) #rounds a number down
+print(result)
