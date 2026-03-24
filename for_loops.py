67aea8e0a05ac9354bec2bd92827326be6b31efd
cygieh <binyanyadeborah770@gmail.com>
2026-03-24 16:26:12 +0300

    for-loops and django

---
+++ for_loops.py
@@ -0,0 +1,7 @@
+# for loops: it will execute a block of code over a fixed number of times.
+#            you can iterate over a range, string,sequence, etc.
+
+
+# count to 10
+for x in reversed(range(1, 11, 2)):
+    print(x)
